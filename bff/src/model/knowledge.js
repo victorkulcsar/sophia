0fe06e2a2e8ff495e2454f06c79aa3df3358a745
Laercio Nogueira <laerciopesa30@hotmail.com>
2020-08-23 05:45:52 -0300

    create methods by graphics

--- bff/src/model/knowledge.js
+++ bff/src/model/knowledge.js
@@ -78,6 +78,113 @@ export default {
 
   },
   
+  _reduceKnowledge(data, area, conhecimento) {
+    return data.reduce((acc, atual) => {
+      if (atual.area === area && atual.conhecimento === conhecimento) {
+        console.log(atual)
+        acc++
+      }
+      return acc;
+    }, 0)
+  },
+
+  knowledgeByAreaByUser (id) {
+    const data = this.selectKnowlegeByUser(id)
+
+    return [
+      {
+        name: 'Domino',
+        data: [
+          (
+            this._reduceKnowledge(data, 'BackEnd', 3) +
+            this._reduceKnowledge(data, 'Arquitetura', 3) +
+            this._reduceKnowledge(data, 'Geral', 3)
+          ),
+          this._reduceKnowledge(data, 'FrontEnd', 3),
+          this._reduceKnowledge(data, 'DevOps', 3),
+        ]
+      }, {
+        name: 'Sei bem',
+        data: [
+          (
+            this._reduceKnowledge(data, 'BackEnd', 2) +
+            this._reduceKnowledge(data, 'Arquitetura', 2) +
+            this._reduceKnowledge(data, 'Geral', 2)
+          ),
+          this._reduceKnowledge(data, 'FrontEnd', 2),
+          this._reduceKnowledge(data, 'DevOps', 2),
+        ]
+      }, {
+        name: 'Sei pouco',
+        data: [
+          (
+            this._reduceKnowledge(data, 'BackEnd', 1) +
+            this._reduceKnowledge(data, 'Arquitetura', 1) +
+            this._reduceKnowledge(data, 'Geral', 1)
+          ),
+          this._reduceKnowledge(data, 'FrontEnd', 1),
+          this._reduceKnowledge(data, 'DevOps', 1),
+        ]
+      }, {
+        name: 'Não sei',
+        data: [
+          (
+            this._reduceKnowledge(data, 'BackEnd', 0) +
+            this._reduceKnowledge(data, 'Arquitetura', 0) +
+            this._reduceKnowledge(data, 'Geral', 0)
+          ),
+          this._reduceKnowledge(data, 'FrontEnd', 0),
+          this._reduceKnowledge(data, 'DevOps', 0),
+        ]
+      }
+    ]
+  },
+
+  monthlyEvolutionByAreaByUser (id) {
+    const { knowledge } = users.selectByUser(id)
+
+     const monthly = knowledge.map(item => {
+      const data = Object.keys(item)[0].split('-')
+      return moment(data[0], 'MM').format('MMM')
+    })
+
+    const values = knowledge.map(item => {
+      const data = Object.keys(item)[0]
+      return Object.values(item[data]).reduce((acc, atual) => {
+        return acc + atual;
+      }, 0)
+    })
+
+    return {
+      series: [{
+        name: 'Conhecimento',
+        data: values
+      }],
+      options: {
+        xaxis: {
+          categories: monthly
+        }
+      }
+    }
+  },
+
+  monthlyEvolutionByUser(id) {
+    const { knowledge } = users.selectByUser(id)
+
+    const values = knowledge.map(item => {
+      const data = Object.keys(item)[0]
+      return Object.values(item[data]).reduce((acc, atual) => {
+        return acc + atual;
+      }, 0)
+    })
+
+    if (values.length === 1) {
+      return 0
+    }
+
+    return (values.slice(-2)[0] / values.slice(-1)[0]).toFixed(1)
+  },
+
   updateKnowlageByUser(userId, id, nivel) {
     
     const data = users.selectAllUsers().map(user => {
